ed35a341a7d9cccc13a8ebec216a7c505d94c183
Luis Marin <luismarinaray@gmail.com>
2015-06-05 18:05:18 -0430

    Modificacion Final Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+
+router.get('/author', function(req, res){
+   res.render('author', { title: 'Quiz' });  
+});
+
+
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
