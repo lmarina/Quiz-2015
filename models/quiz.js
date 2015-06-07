d9aa39f3009e558f93066a88898a5972f7813ffb
Luis Marin <luismarinaray@gmail.com>
2015-06-06 22:22:01 -0430

    Sequelize y SQLite3 correciones menores 4

--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,6 @@
 // Define el modelo Quiz
 
 module.exports = function(sequelize, DataTypes) {
->>>>>>> 5c2f35ffd2f12f52d13e9fa7fcf6ba1163601f72
    return sequelize.define('Quiz',
              { pregunta: DataTypes.STRING,
                respuesta: DataTypes.STRING,
