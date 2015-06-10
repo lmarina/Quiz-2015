4241ff43ee425ace22521519e7b43ad928f4b438
Luis Marin <luismarinaray@gmail.com>
2015-06-10 10:25:47 -0430

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -36,13 +36,15 @@ exports.Quiz = Quiz; // exportar definicion de tabla Quiz
 //Sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
    // then(..) ejecuta el manejador una vez creada la tabla
-   Quiz.count().success(function(count){
+   Quiz.count().then(function(count){
      if(count === 0) {  //la tabla se inicializa solo si esta vacia
        Quiz.create({ pregunta: 'Capital de Italia',
                      respuesta: 'Roma'
-                   })
-       .success(function(){console.log('Base de datos inicializada')});
+                   });
+       Quiz.create({ pregunta: 'Capital de Portugal',
+                                 respuesta: 'Lisboa'
+                               })
+       .then(function(){console.log('Base de datos inicializada')});
 };
 });
 });
-
