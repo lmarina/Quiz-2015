07c9971124c2d34a6719cb2f5b768f3c016d030f
Luis Marin <luismarinaray@gmail.com>
2015-07-03 15:46:17 -0430

    Entregra Final Beta 3

--- controllers/estadisticas_controller.js
+++ controllers/estadisticas_controller.js
@@ -1,27 +1,61 @@
+
 var models = require('../models/models.js');
 
 exports.listar = function(req, res) {
-
     var errors = req.session.errors || {};
-
     var instancia1 = models.Quiz;
-    
     var instancia2 = models.Comment;
 
+    // Funciona Cuenta de Cuantos Quizes
+    /*
+    instancia1.count().then(function(c){ console.log("There are "+c+" registers for Quiz")});
+    */
 
-    instancia1.count().then(function(c){ console.log("There are "+c+" registers")});
+    // Funciona Cuenta de Cuantos Commentarios
+    /*
+    instancia2.count().then(function(d){ console.log("There are "+d+" registers for Comments")});
+    */
 
-    instancia2.count().then(function(d){ console.log("There are "+d+" registers")});
+    // Funciona Cuenta de comentarios por preguntas
+    /*
+    instancia2.count({ where: {QuizId: 1}}).then(function(i){ console.log("There are "+i+" i")});
+    instancia2.count({ where: {QuizId: 2}}).then(function(j){ console.log("There are "+j+" j")});
+    instancia2.count({ where: {QuizId: 3}}).then(function(k){ console.log("There are "+k+" k")});
+    */
 
-    instancia1.count({ where: ['contenido ?']}).then(function(e){ console.log("There are "+e+" question without comments")});
+    // Funciona
+    /*
+    instancia1.all().then(function(projects) {
+          console.log(projects);
+    })
+    */
 
-    //console.log(instancia2);
+/*
+    instancia1.findAll({include:instancia2}).then(function(comments){
+      console.log(JSON.stringify(comments))
+    })
+*/
 
-	//res.render('views/estadisticas.ejs',{errors: errors});
-     
-     //res.redirect("/estadisticas.ejs");
+  // Inner Join entre Quizes y Comentarios
 
+    instancia1.findAll({include: [{model:instancia2, required:true}]}).then(function(quizes){
+      // Convertirlo a objeto Json
+       var Ajson = JSON.stringify(quizes);
+       var Dejson = JSON.parse(Ajson);
+
+      // Contar numero de comentarios totales
+
+        var x; newcount = 0;
+        for (x = 0; x < Object.keys(Dejson).length; x++){
+
+            for (Comments in Dejson[x].Comments) {
+                newcount++;
+            }
+        }
+
+        console.log(newcount);
+
+    //res.render('views/estadisticas.ejs',{errors: errors});
+    //res.redirect("/estadisticas.ejs");
+    })
 };
-
-
-
